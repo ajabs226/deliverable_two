23a2442d3d9a3015779e6a6382e238f82dffe9f3
ajabs226 <aj_abs226@yahoo.com>
2022-03-16 14:36:12 -0400

    updated correctCount tracking

--- src/CoinFlip.java
+++ src/CoinFlip.java
@@ -5,41 +5,33 @@ public class CoinFlip {
 	public static void main(String[] args) {
 		
 		Scanner scnr = new Scanner(System.in);
-		String headsOrTailsGuess;
-		int numberOfFlips = 0;
 		double correctCount = 0;
-		int heads = 0;
-		int tails = 0;
-		
+				
 		System.out.println("Choose Heads or Tails");
-		headsOrTailsGuess = scnr.next().toUpperCase();
-		if(headsOrTailsGuess.equalsIgnoreCase("HEADS") || headsOrTailsGuess.equalsIgnoreCase("TAILS")) {
-		} else {
-			System.out.println("That is not a valid input.  Please try again.");
-			System.exit(0);
-		} 
-					
-		System.out.println("Choose the amount of flips.");
-		try {
-			numberOfFlips = scnr.nextInt();
-		} catch (Exception e) {
-			System.out.println("That is not a valid input.  Please try again.");
-			System.exit(0);
-		}
+		String headsOrTailsGuess = scnr.next();
 		
-		for (var i = 0; i < numberOfFlips; i++) {
-			if(Math.random() >.5) {
-				heads++;	
-				System.out.println("HEADS");
-				correctCount = heads;				
-			} else {
-				tails++;
-				System.out.println("TAILS");
-				correctCount = tails;
-			} 
+		System.out.println("How many flips?");
+		int numberOfFlips = scnr.nextInt();
+		
+		for (int i = 0; i < numberOfFlips; i++) { 
+						
+			String result;
+			
+			if ((int) Math.round(Math.random()) == 0 ) {
+				result = "HEADS";
+			} 	else {
+				result = "TAILS";
+			}
+			
+			System.out.println(result);
+			
+			if (result.equalsIgnoreCase(headsOrTailsGuess)) {
+				correctCount++;
+			}
 			
 		} 
-		double percent =  correctCount/numberOfFlips * 100;
-		System.out.print("Your guess, " + headsOrTailsGuess + ", was correct " + String.format("%.0f", correctCount) + " times. That is " + String.format("%.2f", percent) + "% right!");
+
+		System.out.print("Your guess, " + headsOrTailsGuess + ", was correct " + (int)correctCount + " times. That is " + String.format("%.2f", ((correctCount/numberOfFlips) * 100)) + "% right!");
+		
 	}
-}
+}	
